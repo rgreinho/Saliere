429a3771d910ecc562549c5740d85e17034a53da
Rémy Greinhofer <regreinh@cisco.com>
2015-07-22 16:40:50 -0500

    Fix vagrant-ansible template

--- data/templates/vagrant-ansible/template/idempotency.sh
+++ data/templates/vagrant-ansible/template/idempotency.sh
@@ -4,11 +4,12 @@ VM={{ vm_name }}
 PLAYBOOK=ansible/provision.yml
 
 echo "----------------------------------"
-echo " Creating and provisioning the VM
+echo " Creating and provisioning the VM"
 echo "----------------------------------"
 vagrant destroy -f ${VM}
 vagrant up ${VM}
 echo "------------------------------------------------"
-echo " Replaying the playbook to test for idempotency
+echo " Replaying the playbook to test for idempotency"
 echo "------------------------------------------------"
 ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=.vagrant/machines/${VM}/virtualbox/private_key -u vagrant -b ${PLAYBOOK} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+
