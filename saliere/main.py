8d23efc7fa3bb977da0fce5df875ea094aacab74
Rémy Greinhofer <regreinh@cisco.com>
2015-08-05 10:10:29 -0500

    Fix docopt
    The docopt description section had not been updated properly.

--- saliere/main.py
+++ saliere/main.py
@@ -6,13 +6,15 @@ usage:
   saliere [-hlv]
   saliere <type> <name> [-c FILE] [-o DIR] [--var VARS]
 
+arguments:
+  type                  the type of your template or the path of a jinja template
+  name                  the name of your project
+
 options:
   -c FILE               specify the template configuration file
   -h --help             show this help message and exit
   -l --list             list the available templates
-  -n NAME --name=NAME   set the name of your project
   -o DIR --output=DIR   specify the output directory [default: ./]
-  -t TYPE --type=TYPE   specify the type of your template or the path of a jinja template
   -v --version          show the version information
   --var VARS            define the template variables to use
 """
